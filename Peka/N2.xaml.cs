8864bdd99d7d85cd27f15c8965b8c0aa69632f67
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2021-04-25 15:23:01 +0300

    test
    test

---
+++ Peka/N2.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Peka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class N2 : Window
+    {
+        VHODEntities db = new VHODEntities();
+        public N2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            SingUp SingUp = new SingUp();
+            SingUp.ShowDialog();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            VHOD emp = db.VHOD.SingleOrDefault(c => c.Login == Login.Text);
+            if (emp == null)
+            {
+                MessageBox.Show("Такого пользователя не существует");
+                return;
+            }
+            Func f = new Func();
+            if (f.CheckPassword(emp.Password, f.GetHashPassword(Password.Password)))
+            {
+                MessageBox.Show($"Здравствуйте, {emp.Login}");
+                N4 N4 = new N4();
+                this.Close();
+                N4.Show();
+            }
+            else
+            {
+                MessageBox.Show("Пароль неверный!");
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {    
+            MainWindow c = new MainWindow();
+            c.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Peka/SingUp.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Peka
+{
+    /// <summary>
+    /// Логика взаимодействия для SingUp.xaml
+    /// </summary>
+    public partial class SingUp : Window
+    {
+        VHODEntities db = new VHODEntities();
+        public SingUp()
+        {
+            InitializeComponent();
+        }
+
+        private void BReg_Click(object sender, RoutedEventArgs e)
+        {
+            Func f = new Func();
+            VHOD emp = new VHOD
+            {
+                Login = Email.Text,
+                Password = f.GetHashPassword(PS1.Password),
+            };
+            try
+            {
+                db.VHOD.Add(emp);
+                db.SaveChanges();
+
+
+            }
+            catch (System.Data.Entity.Infrastructure.DbUpdateException)
+            {
+                db.VHOD.Remove(emp);
+                db.SaveChanges();
+                Email.Clear();
+                PS1.Clear();
+                MessageBox.Show("Логин существует");
+                return;
+            }
+           
+            MessageBoxResult res = MessageBox.Show("Человек зарегестрирован! Повторить?", "Добавление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.No)
+            {
+                MainWindow c = new MainWindow();
+                c.Show();
+                this.Close();
+            }
+            else
+            {
+                Email.Clear();
+                PS1.Clear();
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow c = new MainWindow();
+            c.Show();
+            this.Close();
+        }
+    }
+}
